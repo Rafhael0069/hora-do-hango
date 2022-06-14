c61435665c3b0b2953600fbd16f93c75a3724ba8
RafhaelGaspar <86062361+RafhaelGaspar@users.noreply.github.com>
2022-06-13 21:42:44 -0300

    Inicialização do layaut da tela de login

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Hora do rango - 13-06-2022
+<h3>Hora do rango - 13-06-2022</h3>
 
-App voltado para o aprendisado de desenvolvimente de aplicação híbrida com Ionic 6 e Angular 14
+App voltado para o aprendisado de desenvolvimente de aplicação híbrida com Ionic 6 e Angular 14.
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'create-user',
     loadChildren: () => import('./pages/create-user/create-user.module').then( m => m.CreateUserPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'perfil',
