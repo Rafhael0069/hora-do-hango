07a0d1475ff1b15f7bc75e9ba71c839dbdcc0731
RafhaelGaspar <86062361+RafhaelGaspar@users.noreply.github.com>
2022-06-27 20:07:04 -0300

    adicionando projeto e apk android

---
+++ android/app/src/main/java/com/rafhael/dev/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rafhael.dev;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.rafhael.dev',
   appName: 'Hora do Rango',
   webDir: 'www',
   bundledWebRuntime: false
